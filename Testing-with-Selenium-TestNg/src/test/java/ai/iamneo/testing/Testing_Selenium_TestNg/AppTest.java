8997a7fbfbd8b354994b3dc0ccb9df6eb24e9083
Iamneo <techadmin@iamneo.ai>
2023-09-06 07:07:09 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -1,46 +1,60 @@
 package ai.iamneo.testing.Testing_Selenium_TestNg;
+
 import org.testng.annotations.Test;
 import java.net.URL;
 import java.util.List;
-import java.io.IOException;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
-import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class AppTest {
+
 	ChromeOptions chromeOptions = new ChromeOptions();
 	WebDriver driver = null;
+	String url="https://flipkart.com";
 
 	@BeforeTest
 	public void beforeTest() throws Exception {
 		
-		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
 	}
+
 	@Test
-	public void testcase_1() throws InterruptedException
+	public void testcase_1() throws InterruptedException //Go to Ebay
 	{
-		Thread.sleep(3000);
 		driver.manage().window().maximize();
-		driver.get("https://flipkart.com");
+		driver.get(url);
 		List<WebElement> allLink = driver.findElements(By.tagName("a"));
 		System.out.println("Number of links on a page :: "+allLink.size());
+		Assert.assertTrue(true);
 	}
+
 	@Test
 	public void testcase_2() throws InterruptedException 
       {
-		Thread.sleep(3000);
-		List<WebElement> allLink = driver.findElements(By.tagName("a"));
-		for(WebElement link:allLink){
-			System.out.println("Link Text : "+ link.getText());
-			System.out.println("Link URL  : "+ link.getAttribute("href"));
-		}
+	       //write Your Code here to Login
+		   Thread.sleep(3000);
+		   boolean isEnabled = driver.findElement(By.xpath("/html/body/div[3]/div/span")).isDisplayed();
+		   if(isEnabled) {
+			   driver.findElement(By.xpath("/html/body/div[3]/div/span")).click();
+		   }
+		   List<WebElement> allLink = driver.findElements(By.tagName("a"));
+		   for(WebElement link:allLink){
+			   System.out.println("Link Text : "+ link.getText());
+			   System.out.println("Link URL  : "+ link.getAttribute("href"));
+		   }
+		   Assert.assertTrue(true);
 	}
+
+		
 	@AfterTest
 	public void afterTest() {
 		driver.quit();
 	}
+
 }
