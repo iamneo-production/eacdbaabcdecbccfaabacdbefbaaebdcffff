0acc52689e3a94834437daf63b80eeaa3099553e
Iamneo <techadmin@iamneo.ai>
2023-09-06 06:15:52 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -1,56 +1,56 @@
 package ai.iamneo.testing.Testing_Selenium_TestNg;
-
 import org.testng.annotations.Test;
 import java.net.URL;
 import java.util.List;
-import org.openqa.selenium.By;
+import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
-import org.testng.Assert;
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterTest;
 
 public class AppTest {
-
 	ChromeOptions chromeOptions = new ChromeOptions();
 	WebDriver driver = null;
+	
 
 	@BeforeTest
 	public void beforeTest() throws Exception {
 		
-		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
 	}
 
-	@Test
-	public void testcase_1() throws InterruptedException //Go to Ebay
+
+    @Test
+	public void testcase_1() throws InterruptedException
 	{
-		driver.get("https://www.flipkart.com");
-		List<WebElement> links = driver.findElements(By.tagName("a"));
-		System.out.println("Number of links on a page: " + links.size());
-		
-	}
+        // Navigate to Flipkart website
+        driver.get("https://www.flipkart.com");
 
+        // Find all the links on the page
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        // Print the count of links
+        System.out.println("Number of links on a page: " + links.size());
+	}
 	@Test
 	public void testcase_2() throws InterruptedException 
       {
-	       //write Your Code here to Login
-		   List<WebElement> links = driver.findElements(By.tagName("a"));
+        // Print link text and URLs
+		driver.get("https://www.flipkart.com");
+		List<WebElement> links = driver.findElements(By.tagName("a"));
 
-		   for (WebElement link : links) {
-			   System.out.println("Link Text: " + link.getText());
-			   System.out.println("URL: " + link.getAttribute("href"));
-		   }
-	   }
-   
-	
+        for (WebElement link : links) {
+            System.out.println("Link Text: " + link.getText());
+            System.out.println("URL: " + link.getAttribute("href"));
+        }
+    }
 
-		
-	@AfterTest
-	public void afterTest() {
-		driver.quit();
-	}
-
+    @AfterTest
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
 }
-
